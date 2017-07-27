2c5cf4b4ca3127fc6b42339d3ccd8b866a965978
Evans-Munatsa <gagamel.em@gmail.com>
2017-07-27 12:53:59 +0200

    gem faker

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 20.times do
+# 	User.create([{
+# 		name: Faker::Name.name, 
+# 		email: Faker::Internet.email,
+# 		}])
+# end
+
+50.times do |index|
+  User.create!(
+  	name: Faker::Name.name,
+  	surname: Faker::Name.name,
+  	username: Faker::Name.name,
+  	avatar: Faker::Avatar.image,
+    email: Faker::Internet.email,
+    password: Faker::Number.number(10)
+                       )
+end
